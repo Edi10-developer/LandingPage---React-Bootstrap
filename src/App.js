4de65643b859bb3dde0731925d10fa83a301c452
Edi <selimi.edi.1991@gmail.com>
2021-04-07 20:47:39 +0200

    Working in Jumbotron
    I added Layout, NavigationBar and Junbotron components.
    I need to complete Junbotron component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home, About, Contact, NoMatch } from './pages/index';
+import { Layout, NavigationBar } from './components/index';
+
 const App = () => {
   return (
     <>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
+        <NavigationBar />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
       </Router>
     </>
   );
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import boatImage from '../assets.boat.jpg';
+
+const Styles = styled.div``;
+
+const Jumbotron = () => {
+    return (
+        <Styles>
+            
+        </Styles>
+    )
+}
+export default Jumbotron;
